11bd2b49bbb1fa7bd676ba0443643b27b3782402
damjan <joveski.damjan@uklo.edu.mk>
2024-07-11 14:58:11 +0200

    home page edit

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -41,7 +41,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
         $_SESSION["last_regeneration"] = time(); 
 
-        header("Location: ../index.php?page=myBlogs");
+        header("Location: ../index.php?page=home");
         exit();
     } catch (PDOException $e) {
         die("Query failed: " . $e->getMessage());
--- views/home.php
+++ views/home.php
@@ -2,6 +2,22 @@
 session_start(); 
 
 $isLoggedIn  = isset($_SESSION["user_id"]);
+
+require_once "includes/dbh.inc.php";
+
+function get_posts() {
+    global $pdo; 
+
+    $query = "SELECT * FROM posts";
+    $stmt = $pdo->prepare($query);
+    $stmt->execute();
+
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    return $result;
+}
+
+$myPosts = get_posts();
+
 ?>
 
 <!DOCTYPE html>
@@ -19,12 +35,31 @@ $isLoggedIn  = isset($_SESSION["user_id"]);
     if($isLoggedIn){ ?>
         <form action="includes/logout.php" method="POST" class="logout-form">
             <button type="submit" name="logout-submit">Logout</button>
-        </form> <?php
-
+        </form>
+        <form class="myblogs-form">
+            <a href="?page=myBlogs" type="submit" name="myBlogs">My Blogs</a>
+        </form>
+        <div class="blogs"> <?php
+        if(!empty($myPosts)){
+            foreach ($myPosts as $post) { ?>
+                <div class="blog-post">
+                    <?php
+                echo "<h2>{$post['title']}</h2>";
+                echo "<p>{$post['content']}</p>";
+                echo "<p>{$post['author']}</p>";
+                 ?>
+                </div>
+                <?php
+            }
+        } else {
+            echo "<h2>No posts found</h2>";
+        } ?>
+        </div>
+        <?php
     } else { ?>
+        <p>Please login or register to view and write blogs</p>
         <p><a href="?page=register">Register</a> | <a href="?page=login">Login</a></p> <?php
     }
     ?>
-    
 </body>
 </html>
